b0c6c6aaf7c529a186173dfc1ba357400553dabf
Mateus Silva <gmateusjose6@gmail.com>
2023-04-24 18:12:50 -0300

    adding explicit label

--- auditlog/apps.py
+++ auditlog/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 class AuditlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'auditlog'
+    label = 'auditlog'
 
     def ready(self):
         import auditlog.signals  # noqa
